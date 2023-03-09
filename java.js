d9601dc8a7d0eeaf5487f918c99006b731c21def
ValeriaGarNi <118896987+ValeriaGarNi@users.noreply.github.com>
2023-03-09 10:11:27 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var button = document.createElement('input');
     button.type = 'button';
     button.id = 'submit';
-    button.value = 'Submit';
+    button.value = 'Cambiar Fondo';
     button.className = 'btn';
  
     button.onclick = function() {
+        background-image: url("https://static.wikia.nocookie.net/mariokart/images/e/e0/MK8-DLC-Course-BigBlue-overview.jpg/revision/latest?cb=20160307011015");
+  color: #2d568c;
     // …
     };
  
